71f8ffe41f9cbea9cb778aed728080c47b71c449
Botirjon <thedavlativich@gmail.com>
2024-03-04 18:16:19 +0500

    Add generated feedbacks

--- Relevance-Analysis/Relevance-Analysis/obj/Debug/net8.0/Relevance-Analysis.AssemblyInfo.cs
+++ Relevance-Analysis/Relevance-Analysis/obj/Debug/net8.0/Relevance-Analysis.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Relevance-Analysis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a81544bb7052017524d804a601a91fdbf3d1793e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8ed9c6dbf0d4410aa367cba3d601b56b0de57a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Relevance-Analysis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Relevance-Analysis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
